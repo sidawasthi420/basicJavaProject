2374b7b1e8d1b7b1ce8c7fe9451064defbb088a6
Tariqahmad8057 <tariqahmad8057@gmail.com>
2026-07-28 22:59:48 +0530

    array programs

--- src/Tariq/Programs/Programs.java
+++ src/Tariq/Programs/Programs.java
@@ -18,12 +18,12 @@ public class Programs {
 	}
 
 	public void P01_FindLargestElementInArray() {
-		int[] arr = { 50, 2, 6, 8, 2, 10, 6 };
+		int[] arr = {2, 6, 8, 50, 2, 10, 6 };
 		int largest = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] < largest) {
-				arr[i] = largest;
+			if (arr[i] > largest) {
+				largest = arr[i];
 			}
 		}
 		System.out.println(largest);
