a1f5c778bcdd9b9a215814d48e53d98f772596e2
Ravi Raj <ravi9066@gmail.com>
2026-07-29 15:39:37 +0530

    Ravi Raj java programs some are pending

---
+++ src/Ravi-Raj/mergeArray.java
@@ -0,0 +1,20 @@
+package RaviRaj;
+
+public class mergeArray { //Merge Two Arrays
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {11, 22, 35};
+        int[] arr2 = {24, 53, 64};
+
+        System.out.print("Merged Array: ");
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
